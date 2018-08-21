0c66691995a50bc68ce956b7eb3e7e81611b7742
Chris La Spina <christopher.laspina@my.jcu.edu.au>
2018-08-21 15:42:40 +1000

    Add 2 more programs/files

---
+++ prac_04/quick_pick.py
@@ -0,0 +1,11 @@
+import random
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for number in range(1, number_of_quick_picks + 1):
+        for i in range(1, 7):
+            print(random.randint(1, 45), end=" ")
+
+
+main()
