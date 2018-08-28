b69bc97afa6e6e0f771fa570f00f164d9a1c936b
Chris La Spina <christopher.laspina@my.jcu.edu.au>
2018-08-28 16:18:26 +1000

    test

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,15 @@
+def main():
+    counted_phrase = {}
+    phrase = input("Enter a phrase: ")
+    print("Text:", phrase)
+    phrase = phrase.split()
+    for word in phrase:
+        counts = phrase.count(word)
+        counted_phrase[word] = counts
+
+    for word, count in counted_phrase.items():
+        # print(word, ":", count)
+        print("{:{}} : {}".format(word, 8, count))
+
+
+main()
